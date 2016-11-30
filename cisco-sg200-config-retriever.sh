53d36dee4b4cd2a052f0d1b311857ed2af1d6d98
Walter Huf <hufman@gmail.com>
2016-11-30 11:09:16 -0800

    Fixes a bug in downloading backups repeatedly (#1)
    * Fixes the initial spacing
    
    * Uses a random index when generating the backup
    
    The Cisco SG200 series stores backups in an indexed location,
    and that index is used to show a progress bar in the main webpage
    while the download happens in the background.
    This index needs to change for each download.
    
    * Chases any redirects given by the server
    
    The switch seems to use the first page to prepare the download slot,
    and then the client is told to redirect to actually download the file
    
    * Factors out the username and password to variables

--- cisco-sg200-config-retriever.sh
+++ cisco-sg200-config-retriever.sh
@@ -1,53 +1,65 @@
- #!/bin/bash
- # For: Cisco Small Business SG200 26/50 port switch
- # v.0.0.1 09/29/2014
+#!/bin/bash
+# For: Cisco Small Business SG200 26/50 port switch
+# v.0.0.1 09/29/2014
 
- # Show help if first argv does not exist
- IP=$1
- if [ -z ${IP} ]; then
-     echo "Error: IP missing on CLI"
-     echo "Usage: ./$(basename $0) xxx.xxx.xxx.xxx"
-     exit
- fi
+# Show help if first argv does not exist
+IP=$1
+if [ -z ${IP} ]; then
+    echo "Error: IP missing on CLI"
+    echo "Usage: ./$(basename $0) xxx.xxx.xxx.xxx"
+    exit
+fi
 
- USERAGENT="'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36'"
+USERNAME=cisco
+PASSWORD=XXXXXXPASSWORDXXXXXX
 
- # Referer line
- REF="'http://${IP}/cs34055c2b/config/log_off_page.htm'"
+USERAGENT="'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36'"
 
- INITIALCOOKIE="'cisco_numberOfEntriesPerPage=50; pg=00000000000000000000000000000000000000000000000000000000000; isStackableDevice=false; userStatus=ok; activeLangId=English; firstWelcomeBanner=false'"
+# Referer line
+REF="'http://${IP}/cs34055c2b/config/log_off_page.htm'"
 
- # Unencrypted login line. Encrypted uses some long convoluted RSA encryption scheme written in Javascript. I tried for hours to parse it but couldn't figure it out. Maybe another set of eyes will have better luck.
- LOGIN="'http://${IP}/cs34055c2b/config/System.xml?action=login&user=cisco&password=XXXXXXXXXXXXXXXXXXXXXXXX&ssd=true&'"
+INITIALCOOKIE="'cisco_numberOfEntriesPerPage=50; pg=00000000000000000000000000000000000000000000000000000000000; isStackableDevice=false; userStatus=ok; activeLangId=English; firstWelcomeBanner=false'"
 
- # Get the headers for the session so we can parse the output and build our real session string
- SESSION=$(eval curl -i -s -k -b ${INITIALCOOKIE} -A ${USERAGENT} -e ${REF} ${LOGIN} | grep "sessionID")
+# Unencrypted login line. Encrypted uses some long convoluted RSA encryption scheme written in Javascript. I tried for hours to parse it but couldn't figure it out. Maybe another set of eyes will have better luck.
+LOGIN="'http://${IP}/cs34055c2b/config/System.xml?action=login&user=${USERNAME}&password=${PASSWORD}&ssd=true&'"
 
- # UserID is the IP of the machine running this script
- USERID=$(echo ${SESSION} | cut -d'&' -f1 | cut -d'=' -f2)
- SESSIONID=$(echo ${SESSION} | cut -d'&' -f2)
+# Get the headers for the session so we can parse the output and build our real session string
+SESSION=$(eval curl -i -s -k -b ${INITIALCOOKIE} -A ${USERAGENT} -e ${REF} ${LOGIN} | grep "sessionID")
 
- # Newer cookie that gets set in the SG200 once authenticated
- MYCOOKIE="'cisco_numberOfEntriesPerPage=50; pg=00000000000000000000000000000000000000000000000000000000000; isStackableDevice=false; userStatus=ok; sessionID=UserId=${USERID}&${SESSIONID}&; usernme=cisco; activeLangId=English; firstWelcomeBanner=false'"
+# UserID is the IP of the machine running this script
+USERID=$(echo ${SESSION} | cut -d'&' -f1 | cut -d'=' -f2)
+SESSIONID=$(echo ${SESSION} | cut -d'&' -f2)
 
- # Set the referer line to a new value
- REF="'http://${IP}/cs34055c2b/FileMgmt/maintenance_file_fileUpload_m.htm'"
+# Newer cookie that gets set in the SG200 once authenticated
+MYCOOKIE="'cisco_numberOfEntriesPerPage=50; pg=00000000000000000000000000000000000000000000000000000000000; isStackableDevice=false; userStatus=ok; sessionID=UserId=${USERID}&${SESSIONID}&; usernme=cisco; activeLangId=English; firstWelcomeBanner=false'"
 
- # The rl options here get me an http download of the running config from the switch to my machine
- DLURL="'http://${IP}/cs34055c2b/FileMgmt/stupCfg.ber?rlCopyFreeHistoryIndex=94&&rlCopyDestinationFileType=2&&rlCopyOptionsRequestedSsdAccess=3&&redirect=/device/copyfiles.xml'"
+# Set the referer line to a new value
+REF="'http://${IP}/cs34055c2b/FileMgmt/maintenance_file_fileUpload_m.htm'"
 
- TSTAMP=$(date +%Y%m%d-%H%M%S)
- FILENAME=${IP}-${TSTAMP}-running-config.txt
+# The rl options here get me an http download of the running config from the switch to my machine
+INDEX=$(date +%H%M%S)
+DLURL="'http://${IP}/cs34055c2b/FileMgmt/stupCfg.ber?rlCopyFreeHistoryIndex=${INDEX}&&rlCopyDestinationFileType=2&&rlCopyOptionsRequestedSsdAccess=3&&redirect=/device/copyfiles.xml'"
 
- # Actually download the file
- eval curl -s -k -b ${MYCOOKIE} -A ${USERAGENT} -e ${REF} ${DLURL} -o ${FILENAME}
+TSTAMP=$(date +%Y%m%d-%H%M%S)
+FILENAME=${IP}.new
 
- # Check if the file got downloaded by searching the output for the redirection. If the redirection exists
- # then the file was not downloaded and the output is garbage.
- if grep -qi "This document has moved to" ${FILENAME}
- then
-     echo "Output was trash, try again in 15 minutes."
-     rm -f ${FILENAME}
- else
-     echo "Saved the config for ${IP}"
- fi
+# Actually download the file
+eval curl -s -k -b ${MYCOOKIE} -A ${USERAGENT} -e ${REF} ${DLURL} -o ${FILENAME}
+
+# Check if the file got downloaded by searching the output for the redirection.
+# If the redirection exists, follow the redirect
+while grep -qi "This document has moved to" ${FILENAME} && ! grep -qi "log_off_page" ${FILENAME}
+do
+    REF=${DLURL}
+    DLURL="'"$(grep "This document has moved to" ${FILENAME} | sed 's/.*<a href="\([^"]*\)".*/\1/')"'"
+    eval curl -s -k -b ${MYCOOKIE} -A ${USERAGENT} -e ${REF} ${DLURL} -o ${FILENAME}
+done
+
+# We were redirected to a logoff page and the output is garbage.
+if grep -qi "This document has moved to" ${FILENAME}
+then
+    echo "Output was trash, try again in 15 minutes."
+    rm -f ${FILENAME}
+else
+    echo "Saved the config for ${IP}"
+fi
